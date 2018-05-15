71ca2d361c78d272a52db64d9e814e5334f158ff
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-05-15 19:35:54 -0400

    add button and event listener

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Your First (Several) Asynchronous Requests
 
 [See assignment in Alexa.](https://alexa.bitmaker.co/wdi/67/assignments/2055/latest)
+
+
+Part 0
+1. GET Request
+2. 34 ms
+3. 200 ok
+4. browser request
+
+Part 1
+5. Get Request, ok
+6. Ajax request/ JQuery
--- ajax.js
+++ ajax.js
@@ -2,4 +2,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
   /* Your code goes here */
 
+addEventListener('click',function () {
+
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/',
+    method: 'get',
+
+    dataType: 'text'
+  });
+})
+
+
+
 });
